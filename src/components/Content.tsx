7aacb863b46c03879bd237f5f9f57f08075099a2
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-15 18:00:26 -0300

    style: content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,9 @@
+import './Content.css';
+
 export default function Content() {
   return (
-    <article>
-      <h4>Conteúdo da página</h4>
+    <article className="content">
+      <h1>Conteúdo da página</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, modi?</p>
     </article>
   )
