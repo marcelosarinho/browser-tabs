433baabbf2268388fb410652e7a6a3ccb0926151
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-15 15:36:27 -0300

    feat: tab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Tab from './components/Tab'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <i className="ph ph-browser"></i>
+      <Tab />
     </main>
   )
 }
---
+++ src/components/Tab.tsx
@@ -0,0 +1,10 @@
+import TabIcon from "./TabIcon";
+
+export default function Tab() {
+  return (
+    <div className="tab">
+      <TabIcon />
+      <span>Título</span>
+    </div>
+  )
+}
