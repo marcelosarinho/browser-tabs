188f01c94bd376e8ef5f69e2a1b4559a534594bd
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-17 21:35:42 -0300

    feat: initial logic for tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 import BrowserURL from './components/BrowserURL'
 import Content from './components/Content'
 import Header from './components/Header'
 import Tabs from './components/Tabs'
 
+export type Tab = {
+  index: number;
+  name: string;
+}
+
 function App() {
+  const [tabs, setTabs] = useState<Tab[]>([]);
+
+  function newTab() {
+    console.log(tabs.length);
+  }
+
   return (
     <main>
       <Header />
-      <Tabs />
+      <Tabs tabs={tabs} />
       <BrowserURL />
       <Content />
     </main>
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,14 +1,25 @@
+import type { Tab } from '../App';
 import TabIcon from './TabIcon';
 import './Tabs.css';
 
-export default function Tabs() {
+export default function Tabs(props: { tabs: Tab[], onClick: VoidFunction }) {
+  const { tabs } = props;
+
   return (
     <section className="tabs__header">
       <div className="tabs">
+        {tabs.map((tab: Tab) => (
+          <div key={tab.index} className="tabs__tab">
+            <span>{tab.name}</span>
+            <i className="ph ph-x close"></i>
+          </div>
+        ))}
         <div className="tabs__tab">
           <TabIcon />
           <span>Título</span>
-          <i className="ph ph-x close"></i>
+          <button className="close">
+            <i className="ph ph-x close"></i>
+          </button>
         </div>
         <div className="tabs__tab">
           <TabIcon />
