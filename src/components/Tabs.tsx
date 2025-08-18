eb5cc926deb691b110326698bb29bceee3dabf4b
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-17 21:24:43 -0300

    style: tabs header

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,28 +3,32 @@ import './Tabs.css';
 
 export default function Tabs() {
   return (
-    <section className="tabs">
-      <div className="tabs__tab">
-        <TabIcon />
-        <span>Título</span>
-        <i className="ph ph-x close"></i>
+    <section className="tabs__header">
+      <div className="tabs">
+        <div className="tabs__tab">
+          <TabIcon />
+          <span>Título</span>
+          <i className="ph ph-x close"></i>
+        </div>
+        <div className="tabs__tab">
+          <TabIcon />
+          <span>Título</span>
+          <i className="ph ph-x close"></i>
+        </div>
+        <div className="tabs__tab tabs__tab--active">
+          <TabIcon />
+          <span>Título</span>
+          <i className="ph ph-x close"></i>
+        </div>
+        <div className="tabs__tab">
+          <TabIcon />
+          <span>Título</span>
+          <i className="ph ph-x close"></i>
+        </div>
       </div>
-      <div className="tabs__tab">
-        <TabIcon />
-        <span>Título</span>
-        <i className="ph ph-x close"></i>
-      </div>
-      <div className="tabs__tab tabs__tab--active">
-        <TabIcon />
-        <span>Título</span>
-        <i className="ph ph-x close"></i>
-      </div>
-      <div className="tabs__tab">
-        <TabIcon />
-        <span>Título</span>
-        <i className="ph ph-x close"></i>
-      </div>
-      <i className="ph ph-plus"></i>
+      <button className="tabs__add_button">
+        <i className="ph ph-plus"></i>
+      </button>
     </section>
   )
 }
